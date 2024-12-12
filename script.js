ee946d2320298d8030e1bd0899611b9d79b505f3
Laimonas <laimonas.malakauskas@gmail.com>
2024-12-12 18:16:36 +0200

    add pics icons udate html css js

--- script.js
+++ script.js
@@ -8,18 +8,25 @@ window.addEventListener('load', function () {
     scoreBlock.innerText = score;
   }
 
-  document.querySelectorAll('.factory').forEach(function (fc) {
+  document.querySelectorAll('.factory').forEach(function (fc, index) {
     let factory = {
       title: fc.querySelector('.title').innerText,
       costs: parseInt(fc.querySelector('.price').innerText),
       isAdding: parseInt(fc.querySelector('.adds').innerText),
       count: parseInt(fc.querySelector('.count').innerText.slice(1, -1)),
-      button: fc.querySelector('button')
+      button: fc.querySelector('button'),
+      cursorImage: 'img/cursor-' + (index + 1) + '.png',
+      backgroundImage: 'img/background-' + (index + 1) + '.jpg'
     };
     factory.button.onclick = function () {
       if (score >= factory.costs) {
         score -= factory.costs;
         factory.count++;
+   
+        document.querySelector('.click-zone').style.cursor = `url('img/cursor-${index + 1}.png'), auto`;
+
+        document.querySelector('.main-game .click-zone').style.backgroundImage = `url('img/background-${index + 1}.jpg')`;
+  
         scoreBlock.innerText = score;
         fc.querySelector('.count').innerHTML = '(' + factory.count + ')';
       }
